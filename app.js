2a280b8271d38609b293fd019ce1ed3cbf88f0e6
InfinityBowman <jakebrake115@gmail.com>
2024-03-11 22:01:28 -0600

    Added dummy music page with cool thingy on it

--- app.js
+++ app.js
@@ -17,10 +17,19 @@ const highlightMenu = () => {
     const homeMenu = document.querySelector('#home-page');
     const aboutMenu = document.querySelector('#about-page');
     const dataMenu = document.querySelector('#data-page');
+    const musicMenu = document.querySelector('#music-page');
     let scrollPos = window.scrollY;
 
     if (window.location.href.includes('data.html')) {
         dataMenu.classList.add('highlight');
+        homeMenu.classList.remove('highlight');
+        aboutMenu.classList.remove('highlight');
+        return;
+    }
+
+    if (window.location.href.includes('music.html')) {
+        musicMenu.classList.add('highlight')
+        dataMenu.classList.remove('highlight');
         homeMenu.classList.remove('highlight');
         aboutMenu.classList.remove('highlight');
         return;
@@ -32,21 +41,21 @@ const highlightMenu = () => {
         aboutMenu.classList.remove('highlight');
         return;
     }
-    else if (window.innerWidth > 960 && scrollPos < 1400) {
-        aboutMenu.classList.add('highlight');
-        homeMenu.classList.remove('highlight');
-        dataMenu.classList.remove('highlight');
-        return;
-    }
-    else if (window.innerWidth > 960 && scrollPos < 2345) {
-        dataMenu.classList.add('highlight');
-        aboutMenu.classList.remove('highlight');
-        return;
-    }
+    // else if (window.innerWidth > 960 && scrollPos < 1400) {
+    //     aboutMenu.classList.add('highlight');
+    //     homeMenu.classList.remove('highlight');
+    //     dataMenu.classList.remove('highlight');
+    //     return;
+    // }
+    // else if (window.innerWidth > 960 && scrollPos < 2345) {
+    //     dataMenu.classList.add('highlight');
+    //     aboutMenu.classList.remove('highlight');
+    //     return;
+    // }
 
-    if((elem && window.innerWidth > 960 && scrollPos < 600) || elem) {
-        elem.classList.remove('highlight');
-    }
+    // if((elem && window.innerWidth > 960 && scrollPos < 600) || elem) {
+    //     elem.classList.remove('highlight');
+    // }
 }
 
 window.addEventListener('scroll', highlightMenu);
--- pages.js
+++ pages.js
@@ -27,6 +27,20 @@ document.getElementById('home-page').addEventListener('click', function(event) {
     }
 });
 
+document.getElementById('music-page').addEventListener('click', function(event) {
+    // Prevent the default behavior of the link
+    event.preventDefault();
+
+    // Check if the current page is 'data.html'
+    if (window.location.href.endsWith('music.html')) {
+        // Scroll to the top of the page
+        window.scrollTo(0, 0);
+    } else {
+        // Change the URL and navigate to a new page
+        window.location.href = 'music.html';
+    }
+});
+
 document.getElementById('navbar__logo').addEventListener('click', function(event) {
     // Prevent the default behavior of the link
     event.preventDefault();
